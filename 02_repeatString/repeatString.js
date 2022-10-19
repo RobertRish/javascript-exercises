dc463354ea0404b23c019012a6d9769e3a74bb83
RobertRish <robert.rish1225@gmail.com>
2022-10-19 18:41:13 -0400

    Completed exercises 1 and 2

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,6 +1,17 @@
-const repeatString = function() {
-
+const repeatString = function(string, num) {
+    if (num < 0) return 'ERROR';
+    let newString = '';
+    for (i = 1; i <= num; i++) {
+        newString += string;
+    }
+    return newString;
 };
+
+// npm test repeatString.spec.js
+
+
+
+
 
 // Do not edit below this line
 module.exports = repeatString;
