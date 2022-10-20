32b3ea8fa5940a1d5c2deb183f14edd38518f98e
RobertRish <robert.rish1225@gmail.com>
2022-10-19 21:28:17 -0400

    passed remaining test for exercise 3

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,10 +1,5 @@
-const reverseString = function(string) {
-    let newString = '';
-    for (let i = 1; i <= string.length; i++) {
-        let getLast = string.substr(-i, 1);
-        newString += getLast;
-    }
-    return newString;
+const reverseString = function (string) {
+  return string.split("").reverse().join("");
 };
 
 // npm test reverseString.spec.js
